3419f70d97e1bb9b146d4a0f8127b0c045ae3d67
s-scheck <128038256+s-scheck@users.noreply.github.com>
2023-03-17 11:45:29 +0000

    first file made in codespace

---
+++ madeincodespace.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def printName(self):
+        print(f"Name: {self.name} & Alter: {self.age}" )
+
+person = Person("Hans", 59)
+person.printName()
